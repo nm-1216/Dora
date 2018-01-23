5ea60923b3ee8727ea5a6e5156cb292e92301351
rose1208 <rose1208@sina.cn>
2018-01-23 14:37:01 +0800

    添加service和interface

---
+++ src/VNEXT/Dora.Services/School/AutoCode/Syllabus/SyllabusFirstCourseService.cs
@@ -0,0 +1,14 @@
+﻿using Dora.Domain.Entities.School;
+using Dora.Infrastructure.Infrastructures.Interfaces;
+using Dora.Infrastructure.Services;
+using Dora.Services.School.Interfaces;
+
+namespace Dora.Services.School 
+{
+    public partial class SyllabusFirstCourseService : BaseService<SyllabusFirstCourse>, ISyllabusFirstCourseService
+    {
+        public SyllabusFirstCourseService(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
---
+++ src/VNEXT/Dora.Services/School/AutoCode/Syllabus/SyllabusTeacherService.cs
@@ -0,0 +1,14 @@
+﻿using Dora.Domain.Entities.School;
+using Dora.Infrastructure.Infrastructures.Interfaces;
+using Dora.Infrastructure.Services;
+using Dora.Services.School.Interfaces;
+
+namespace Dora.Services.School
+{
+    public partial class SyllabusTeacherService : BaseService<SyllabusTeacher>, ISyllabusTeacherService
+    {
+        public SyllabusTeacherService(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
