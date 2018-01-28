30848b84491bdb8e4a23166f0da6f80f0e492618
rose1208 <rose1208@sina.cn>
2018-01-28 23:00:57 +0800

    教学任务导入

--- src/VNEXT/Dora.School/Controllers/SyllabusController.cs
+++ src/VNEXT/Dora.School/Controllers/SyllabusController.cs
@@ -10,9 +10,13 @@
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.EntityFrameworkCore;
-    using Microsoft.Extensions.Logging; 
+    using Microsoft.Extensions.Logging;
+    using NPOI.HSSF.UserModel;
+    using NPOI.SS.UserModel;
+    using NPOI.XSSF.UserModel;
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Threading.Tasks;
 
@@ -31,6 +35,8 @@
         private ICourseService _CourseService;
         private IProfessionalService _ProfessionalService;
         private ITeacherService _TeacherService;
+        private IClassService _ClassService;
+        private ITeachingTaskService _TeachingTaskService;
 
         public SyllabusController(RoleManager<SchoolRole> roleManager, UserManager<SchoolUser> userManager, ILoggerFactory loggerFactory
         , ISyllabusService SyllabusService
@@ -42,7 +48,9 @@
         , IProfessionalService ProfessionalService
         , ICourseService CourseService
         , ITeacherService teacherService
-		, ISyllabusLogService syllabusLogService
+         ,IClassService classService
+         , ITeachingTaskService teachingTaskService
+        , ISyllabusLogService syllabusLogService
         ) : base(roleManager, userManager, loggerFactory)
         {
             _SyllabusService = SyllabusService;
@@ -54,6 +62,7 @@
             _ProfessionalService = ProfessionalService;
             _CourseService = CourseService;
             _TeacherService = teacherService;
+            _TeachingTaskService = teachingTaskService;
         }
 
 
@@ -534,6 +543,189 @@
             }
         }
         #endregion
+
+        public async Task<IActionResult> ImportTeachingTask([FromServices]IHostingEnvironment env, IList<IFormFile> files)
+        {
+            foreach (var file in files)
+            {
+                var fileExtension = Path.GetExtension(file.FileName).ToLower();
+
+                if (!fileExtension.Equals(".xls") && !fileExtension.Equals(".xlsx"))
+                {
+                    return new JsonResult(new AjaxResult("文件格式不正确") { result = 0 });
+                }
+
+                var dir = Path.Combine(env.WebRootPath, "upload", "temp", DateTime.Now.ToString("yyMMdd"));
+                var fileName = Path.Combine(env.WebRootPath, "upload", "temp", DateTime.Now.ToString("yyMMdd"), Guid.NewGuid() + fileExtension).ToLower();
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+                using (FileStream fs = System.IO.File.Create(fileName))
+                {
+                    file.CopyTo(fs);
+                    fs.Flush();
+                }
+
+                IWorkbook workbook = null;
+                ISheet sheet = null;
+                using (var Read = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+                {
+
+                    if (fileExtension.Equals(".xlsx")) // 2007版本
+                        workbook = new XSSFWorkbook(Read);
+                    else
+                        workbook = new HSSFWorkbook(Read);
+
+                    if (workbook != null)
+                    {
+                        sheet = workbook.GetSheet("教学任务");
+                        if (sheet == null)
+                            sheet = workbook.GetSheetAt(0);
+                    }
+
+                    IRow row = sheet.GetRow(0);
+                    var course = GetValue(row.GetCell(0));
+                    var teacher = GetValue(row.GetCell(1));
+                    var term = GetValue(row.GetCell(2));
+                    var classes = GetValue(row.GetCell(3));
+                    var BegWeek = GetValue(row.GetCell(4));
+                    var EndWeek = GetValue(row.GetCell(5));
+                    var ClaRoomCode = GetValue(row.GetCell(6));
+                    var Week = GetValue(row.GetCell(7));
+                    var Section = GetValue(row.GetCell(8));
+                    var Memo = GetValue(row.GetCell(9));
+
+                    if (!course.Equals("课程") || !term.Equals("老师") || !term.Equals("学期")
+                       || !classes.Equals("班级") || !BegWeek.Equals("开始周次") || !EndWeek.Equals("结束周次")
+                       || !ClaRoomCode.Equals("教室编号") || !Week.Equals("星期") || !Section.Equals("上课节次")
+                       || !Memo.Equals("备注"))
+                    {
+                        return new JsonResult(new AjaxResult("EXCEL文件格式不正确") { result = 0 });
+                    }
+
+                    int rowCount = sheet.LastRowNum;
+                    for (int i = 1; i <= rowCount; i++)
+                    {
+                        row = sheet.GetRow(i);
+                        if (row == null) continue;
+
+                        course = GetValue(row.GetCell(0));
+                        teacher = GetValue(row.GetCell(1));
+                        term = GetValue(row.GetCell(2));
+                        classes = GetValue(row.GetCell(3));
+                        BegWeek = GetValue(row.GetCell(4));
+                        EndWeek = GetValue(row.GetCell(5));
+                        ClaRoomCode = GetValue(row.GetCell(6));
+                        Week = GetValue(row.GetCell(7));
+                        Section = GetValue(row.GetCell(8));
+                        Memo = GetValue(row.GetCell(9));
+
+
+                        if (!string.IsNullOrEmpty(course) && !string.IsNullOrEmpty(teacher) && !string.IsNullOrEmpty(term)
+                            && !string.IsNullOrEmpty(classes) && !string.IsNullOrEmpty(BegWeek) && !string.IsNullOrEmpty(EndWeek)
+                            && !string.IsNullOrEmpty(ClaRoomCode) && !string.IsNullOrEmpty(Week) && !string.IsNullOrEmpty(Section))//备注可以为空
+                        {
+                            var model = _CourseService.Find(r => r.CourseId == course);
+                            if (model != null)
+                                continue;
+
+                            //课程(列表)
+                            var Courses = new List<TeachingTaskClass>()
+                                {
+                                    new Domain.Entities.School.TeachingTaskClass()
+                                     {
+                                    CourseId = course
+                                        }
+                                };
+
+                            //教师(多个) 
+                            var teachersList = new List<TeachingTaskTeacher>();
+                            foreach (var tid in teacher.Split(','))
+                            {
+                                var titem = _TeacherService.Find(r => r.TeacherId == tid);
+                                if (titem == null)
+                                    continue;
+
+                                var teachers = new Domain.Entities.School.TeachingTaskTeacher()
+                                {
+                                    TeacherId = tid
+                                };
+                                teachersList.Add(teachers);
+                            }
+
+                            //班级(多个)，在教学任务明细中
+                            var TeachingTaskDetailList = new List<TeachingTaskDetail>();
+                            foreach (var cid in classes.Split(','))
+                            {
+                                var citem = _ClassService.Find(r => r.ClassId == cid);
+                                if (citem == null)
+                                    continue;
+
+                                int index = 1;
+                                var teachingTaskDetail = new Domain.Entities.School.TeachingTaskDetail()
+                                {
+                                    TeachingTaskDetailId = index++,
+                                    ClaRoomCode = cid,
+                                    Week = Week,
+                                    Section = ((SectionType)Convert.ToInt16(Section))
+                                };
+                                TeachingTaskDetailList.Add(teachingTaskDetail);
+                            }
+
+
+                            //教学任务
+                            var TeachingTask = new Domain.Entities.School.TeachingTask()
+                            {
+                                TeachingTaskId = Guid.NewGuid().ToString(),
+                                Term = term,
+                                BegWeek = Convert.ToInt16(BegWeek),
+                                EndWeek = Convert.ToInt16(EndWeek),
+                                ClaRoomCode = ClaRoomCode,
+                                Week = Week,
+                                Section = ((SectionType)Convert.ToInt16(Section)),
+                                Memo = Memo,
+                                Courses = Courses,
+                                Teachers = teachersList,
+                                TeachingTaskDetails = TeachingTaskDetailList
+                            };
+                             
+                            var result = await _TeachingTaskService.Add(TeachingTask);
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                }
+            }
+
+            return new JsonResult(new AjaxResult("导入成功"));
+        }
+
+        private string GetValue(ICell cell)
+        {
+            if (cell == null)
+            {
+                return null;
+            }
+
+            if (cell.CellType == CellType.Boolean)
+            {
+                return cell.BooleanCellValue.ToString();
+            }
+            else if (cell.CellType == CellType.Numeric)
+            {
+                Double doubleVal = cell.NumericCellValue;
+
+
+                return doubleVal.ToString("#.####");
+            }
+            else
+            {
+                return cell.StringCellValue.Trim();
+            }
+        }
 
     }
 }
