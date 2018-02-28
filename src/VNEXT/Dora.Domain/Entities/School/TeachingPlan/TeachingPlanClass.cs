6073dd40361d5560a3a54f1f39d1204b5c765c9e
倾苏恋 <o7o@outlook.com>
2018-02-28 10:56:32 +0800

    修改数据结构

---
+++ src/VNEXT/Dora.Domain/Entities/School/TeachingPlan/TeachingPlanClass.cs
@@ -0,0 +1,28 @@
+﻿namespace Dora.Domain.Entities.School
+{
+    using Dora.Infrastructure.Domains;
+
+    public class TeachingPlanClass : BaseEntity
+    {
+        /// <summary>
+        /// Gets or sets 
+        /// </summary>
+        public string ClassId { get; set; }
+
+        /// <summary>
+        /// Gets or sets 
+        /// </summary>
+        public virtual string TeachingPlanId { get; set; }
+
+        /// <summary>
+        /// Gets or sets 
+        /// </summary>
+        public virtual Class Class { get; set; }
+
+        /// <summary>
+        /// Gets or sets  
+        /// </summary>
+        public virtual TeachingPlan TeachingPlan { get; set; }
+
+    }
+}
---
+++ src/VNEXT/Dora.Domain/Entities/School/TeachingPlan/TeachingPlanTeacher.cs
@@ -0,0 +1,29 @@
+﻿namespace Dora.Domain.Entities.School
+{
+    using Dora.Infrastructure.Domains;
+
+    public class TeachingPlanTeacher : BaseEntity
+    {
+        /// <summary>
+        /// Gets or sets 此授课计划对应的维护人员
+        /// </summary>
+        public virtual string TeacherId { get; set; }
+
+        /// <summary>
+        /// Gets or sets
+        /// </summary>
+        public virtual string TeachingPlanId { get; set; }
+
+        /// <summary>
+        /// Gets or sets
+        /// </summary>
+        public virtual TeachingPlan TeachingPlan { get; set; }
+
+        /// <summary>
+        /// Gets or sets
+        /// </summary>
+        public virtual Teacher Teacher { get; set; }
+
+
+    }
+}
