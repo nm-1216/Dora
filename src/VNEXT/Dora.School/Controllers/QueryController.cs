24feef22b967cf42d8c97e14a4b74471eb1a5a0d
rose1208 <rose1208@sina.cn>
2018-04-09 15:52:37 +0800

    授课计划查询

--- src/VNEXT/Dora.School/Controllers/QueryController.cs
+++ src/VNEXT/Dora.School/Controllers/QueryController.cs
@@ -16,12 +16,18 @@
     public class QueryController : BaseUserController<QueryController>
     {
         private readonly ISyllabusService _SyllabusService;
+        private readonly ITeachingPlanService _TeachingPlanService;
+        private readonly ITeachingPlanDetailService _TeachingPlanDetailService;
 
         public QueryController(RoleManager<SchoolRole> roleManager, UserManager<SchoolUser> userManager, ILoggerFactory loggerFactory
         , ISyllabusService SyllabusService
+        , ITeachingPlanService TeachingPlanService
+        , ITeachingPlanDetailService TeachingPlanDetailService
         ) : base(roleManager, userManager, loggerFactory)
         {
             _SyllabusService = SyllabusService;
+            _TeachingPlanService = TeachingPlanService;
+            _TeachingPlanDetailService = TeachingPlanDetailService;
         }
 
         /// <summary>
@@ -40,6 +46,41 @@
                 .Where(b => string.IsNullOrEmpty(searchKey) || b.CourseId.Contains(searchKey) || b.Course.Name.Contains(searchKey))
                 .OrderBy(o => o.CreateTime)
                 , page, user.PageSize);
+
+            return View(list);
+        }
+
+
+        /// <summary>
+        /// 授课计划查询
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> TeachingPlanList(string searchKey, int page = 1)
+        {
+            ViewData["searchKey"] = searchKey;
+            var user = await GetCurrentUserAsync();
+
+            var list = new PageList<TeachingPlan>(_TeachingPlanService.GetAll()
+                .Include(b => b.Teacher)
+                .Include(b => b.Course)
+                .Where(b => string.IsNullOrEmpty(searchKey) || b.Course.Name.Contains(searchKey))
+                .OrderBy(o => o.CreateTime)
+                , page, user.PageSize);
+
+            return View(list);
+        }
+
+
+
+        /// <summary>
+        /// 授课计划明细
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult TeachingPlanDetail(string id)
+        {
+            var list = _TeachingPlanDetailService.GetAll().Where(r => r.TeachingPlanId == id)
+                .OrderBy(r => r.Order)
+                .ToList();
 
             return View(list);
         }
